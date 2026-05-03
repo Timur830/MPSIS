c7e70e0be55646ab5be78927859040c3272a4df4
Timur830 <tim.general123@gmail.com>
2026-05-03 18:13:10 +0300

    Update alu.sv

--- lab2/alu.sv
+++ lab2/alu.sv
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/07/2025 12:48:24 PM
-// Design Name: 
-// Module Name: alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module alu (
     input  logic [31:0]  a_i,
     input  logic [31:0]  b_i,
